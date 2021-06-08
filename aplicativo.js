77b5b7fc9d040249a76a8ed8f32824f8f0652e77
Gustavo Emanuel Farias Rosa <gustavo.emanuel01@outlook.com>
2021-06-08 17:44:38 -0300

    Pagina pronta

--- aplicativo.js
+++ aplicativo.js
@@ -1,10 +1,18 @@
+
+//Carregar primeiro elemento
+$('.tab-menu1 a').eq(0).addClass('active')
+$('section.animais div.item').eq(0).addClass('active')
+$('section.animais div.item').each(function(){ $(this).css('padding-top', '10px') })
+
 $('.tab-menu1 a').on('click', function(e){
     e.preventDefault();
     var text = 'Titulo editavel';
 
-    var a = $(this).parents().eq(1).find('a')
-
-    console.log(a)
+    var remover_active = function(){
+        $(this).removeClass('active');
+    }
+    $(this).parents().eq(1).find('a').each(remover_active)
+    $('section.animais div.item').each(remover_active)
 
     //Classe no próprio item
     $(this).addClass('active');

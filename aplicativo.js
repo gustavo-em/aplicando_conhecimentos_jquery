b4ab47569def3405cbd335e95b327a1a5b0366b1
Gustavo Emanuel Farias Rosa <gustavo.emanuel01@outlook.com>
2021-06-10 22:36:54 -0300

    Refatoração -Arrumando identação html -Efeito scrol js -Botão basquete

--- aplicativo.js
+++ aplicativo.js
@@ -1,5 +1,5 @@
 
-
+// Evento de deixar opção ativada no menu da outra section
 $('[data-grupo]').each(function(){
 
     var tags_a = $(this).find('[data-item]');
@@ -22,7 +22,6 @@ $('[data-grupo]').each(function(){
             var item_anterior = $('[data-grupo="animais"]').find('[data-item].active');
         }
 
-
         $('[data-conteudo]').removeClass('active')
         var conteudo = $(this).data('item')
         $('[data-conteudo="'+conteudo+'"]').addClass('active');
@@ -35,20 +34,19 @@ $('[data-grupo]').each(function(){
     })
 })
 
-
+// Adicionar div no botão ativar
 $('.botao_ativar').wrap("<div class='acima' data-seila='a'></div>")
 
-
+// Adicionar ou remover classe animar no botão ativar
 $('.botao_ativar').on('click', function(){
-
-    if( $(this).is('.animar') ){
+    if($(this).is('.animar') ){
         $(this).removeClass('animar')
     } else {
         $(this).addClass('animar')
     }
-
 })
 
+// Ao passar mouse por cima faz efeito de basquete
 $('.botao_ativar').on('mouseenter', function(){
     if( $(this).is('.animar_scale') ){
 
@@ -57,66 +55,31 @@ $('.botao_ativar').on('mouseenter', function(){
     }
 })
 
-
-/* $('div.menu-cel').on('click', function(){
-    $('.navegacao').toggle('.nav-cel');
-})
-$('div.menu-cel').each(function(){
-    console.log($(window).width());
-})
-
-console.log(window.screen.width)
- */
-
+// Media querie por JS não utilizada
 document.body.onresize = function() {
     if (document.body.clientWidth < 600) {
-        /* $('.nav-cel').attr('style', 'display: block') */
-        /* $('.navegacao').addClass('nav-cel') */
     }
-    
 };
 
-/* 
+// Esconder Menu Mobile com a classe activee com media querie
 $('.menu-cel').on('click', function(){
-    if($(this).hasClass('nav-cel')) {
-        console.log('aa')
-        $('.navegacao').removeClass('nav-cel')
+    if($('.nav-cel').parent().hasClass('activee')) {
+        $('.nav-cel').parent().removeClass('activee')
     } else {
-        console.log('bb')
-        $('.navegacao').addClass('nav-cel')
-    }
-}) */
-
-$('.menu-cel').on('click', function(){
-    if($('.nav-cel').parent().hasClass('active')) {
-        $('.nav-cel').parent().removeClass('active')
-    } else {
-        $('.nav-cel').parent().addClass('active')
+        $('.nav-cel').parent().addClass('activee')
     }
 })
 
-console.log($(window).scrollTop())
-
-$('[data-anime="scroll"]').each(function(){
-    console.log($(this).offset().top);
-    
-})
-
-
+// Animação no scroll para efeito de transform com opaciti da classe anime
 $(window).on('scroll', function(){
+    // Elementos com data-anime
     $('[data-anime="scroll"]').each(function(){
+        // Pegar posições da tela
         var baixo_item = $(this).offset().top;
-        var altura_scroll_cima =  $(window).scrollTop()
+        var altura_scroll_cima =  $(window).scrollTop()+1000
 
-        console.log(baixo_item)
-        console.log(altura_scroll_cima)
-        
-
-        if(altura_scroll_cima < baixo_item){
-            $(this).addClass('anime')
-        } else {
-            $(this).removeClass('anime')
-        }
+        if(altura_scroll_cima > baixo_item) $(this).addClass('anime')
+               
+        $(this).removeClass('anime')
     })
-
 })
